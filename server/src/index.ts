beb4a4036a7fc9326e720a84dc1c2490810ed0e5
Henry Paulino <me@henrypl.com>
2026-02-05 16:45:59 +0000

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -537,6 +537,6 @@ export async function startServer(port: number) {
 }
 
 if (import.meta.main) {
-	const port = Number(Bun.env.PORT) || 4002
+	const port = Number(Bun.env.PORT) || 4001
 	startServer(port)
 }
